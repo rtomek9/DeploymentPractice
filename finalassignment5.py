51fb01c17fbdcd6ba0e531ab46ffa1f48bb10b6a
rtomek9 <153258867+rtomek9@users.noreply.github.com>
2024-03-21 13:07:29 -0400

    finalassignment5.py

--- finalassignment5.py
+++ finalassignment5.py
@@ -27,7 +27,7 @@ df_long = df.melt(id_vars=df.columns[0],
 # initialize app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 # graph
 df_long['GDP'] = pd.to_numeric(df_long['GDP'], errors='coerce')
 df_long['year'] = pd.to_numeric(df_long['year'], errors='coerce')
